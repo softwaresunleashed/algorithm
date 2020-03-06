52f6a3bb7ff8aaa498221a1b4dd3f873118fbbe4
Softwares Unleashed <softwares.unleashed@gmail.com>
2020-03-06 15:30:07 +0530

    Added - [Updated] 01_InterviewPreparationLinks.txt - Linux Interview Questions - 02_Interview Questions March.pdf - Message Passing using Files - 06_message_passing_using_files - Message Passing using Shared Memory Posix APIs - 07_message_passing_using_shared_memory_posix
    Signed-off-by: Softwares Unleashed <softwares.unleashed@gmail.com>

---
+++ interview_preparation/07_message_passing_using_shared_memory_posix/shmem.h
@@ -0,0 +1,7 @@
+#define AccessPerms	0644
+#define MemContents	"Just Some Random Memory Contents"
+
+
+#define SemaphoreName 	"SharedSemaphore"
+#define ByteSize	512
+#define BackingFile	"shMemExample"
