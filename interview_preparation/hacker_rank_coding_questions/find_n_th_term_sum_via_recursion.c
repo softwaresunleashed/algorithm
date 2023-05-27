4cec0431850170680a0bc212ac859a46a4f8b5e0
Softwares Unleashed <softwares.unleashed@gmail.com>
2023-05-27 21:30:06 +0530

    added hacker rank recursion problem
    https://www.hackerrank.com/challenges/recursion-in-c/problem?isFullScreen=true
    
    Signed-off-by: Softwares Unleashed <softwares.unleashed@gmail.com>

---
+++ interview_preparation/hacker_rank_coding_questions/find_n_th_term_sum_via_recursion.c
@@ -0,0 +1,39 @@
+/*
+ * Question: 
+ * https://www.hackerrank.com/challenges/recursion-in-c/problem?isFullScreen=true
+ */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+  int result = 0;
+  
+  if(n == 1)
+    return a;
+  if(n == 2)
+    return b;
+  if(n == 3)
+    return c;
+
+  result = find_nth_term(n-1, a, b, c) 
+            + find_nth_term(n-2, a, b, c) 
+            + find_nth_term(n-3, a, b, c);
+  
+  return result;
+}
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
